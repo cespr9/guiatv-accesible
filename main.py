5acca678b7ae72be6d16638097cce6ad1ed430af
César Marqués Jiménez <cesar@wifialu16-13.wi-fi.upv.es>
2026-04-25 18:40:33 +0200

    Solucioando problema de accesibilidad

--- main.py
+++ main.py
@@ -5,19 +5,16 @@ from datetime import datetime, timezone, timedelta
 
 st.set_page_config(page_title="Guía TV Accesible", page_icon="📺")
 
-# 1. Función auxiliar de fechas
 def obtener_fecha(cadena_xml):
     fecha_limpia = cadena_xml.split(" ")[0]
     return datetime.strptime(fecha_limpia, "%Y%m%d%H%M%S")
-
-# 2. CACHÉ SOLO PARA LA DESCARGA (Para no saturar GitHub ni ir lentos)
 @st.cache_data(ttl=3600)
+
 def descargar_xml():
     url = "https://raw.githubusercontent.com/davidmuma/EPG_dobleM/master/guiatv.xml"
     respuesta = requests.get(url)
     return respuesta.content
 
-# 3. LÓGICA DE FILTRADO (SIN CACHÉ: Se ejecuta siempre que recargas)
 def procesar_guia():
     contenido_xml = descargar_xml()
     root = ET.fromstring(contenido_xml)
@@ -33,27 +30,20 @@ def procesar_guia():
     "Telemadrid": "Telemadrid"
 
     }
-
-    
     guia_por_dias = {}
     
-    # --- LA MAGIA DEL TIEMPO ---
-    # Pedimos la hora mundial (UTC) y le sumamos 2 horas (Horario de verano España)
-    # Así no importa dónde esté el servidor alojado, la hora será exacta.
     ahora_utc = datetime.now(timezone.utc)
     ahora_espana = ahora_utc + timedelta(hours=2)
-    ahora = ahora_espana.replace(tzinfo=None) # Limpiamos la zona para compararla con el XML
+    ahora = ahora_espana.replace(tzinfo=None)
 
     for programa in root.findall('programme'):
         canal_id = programa.get('channel')
         
         if canal_id in canales_interes:
             nombre_canal = canales_interes[canal_id]
-            
-            # Comprobamos la hora de fin
+           
             fin_dt = obtener_fecha(programa.get('stop'))
             
-            # El Filtro: Si ya ha terminado, saltamos al siguiente
             if fin_dt < ahora:
                 continue
                 
@@ -73,7 +63,6 @@ def procesar_guia():
             
     return guia_por_dias
 
-# --- INTERFAZ WEB ---
 st.title("📺 Guía de TV Accesible")
 
 with st.spinner("Descargando y filtrando la programación actual..."):
@@ -86,8 +75,7 @@ if datos_tv:
     for i, dia in enumerate(dias_disponibles):
         with pestanas[i]:
             for canal, programas in datos_tv[dia].items():
-                # Con expanded=False conseguimos que la lista empiece colapsada y limpia
-                with st.expander(f"📡 {canal}", expanded=False):
+                with st.expander(f"{canal}", expanded=False):
                     st.markdown("\n\n".join(programas))
 else:
     st.error("No hay programación disponible o hubo un error al cargar.")
